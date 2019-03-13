53e27d626ddc75f95c2acb11d7177ecbb0b105c8
Adam <alfredsson.adam@gmail.com>
2019-03-13 10:40:51 +0100

    Make test file ts with import and add cypress to tsconfig types

--- tests/e2e/specs/test.ts
+++ tests/e2e/specs/test.ts
@@ -1,7 +1,10 @@
+import { hello } from "../utils/hello";
+
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('My First Test', () => {
   it('Visits the app root url', () => {
+    cy.log(hello);
     cy.visit('/')
     cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
   })
